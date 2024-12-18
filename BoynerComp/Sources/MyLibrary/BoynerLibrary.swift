92c1eaacdc786efe8af3b74b269db83afb8ccfaf
Tugay <s.tugay.koyunkaya@gmail.com>
2024-12-19 01:42:36 +0300

    Add Boyner Comp

---
+++ BoynerComp/Sources/MyLibrary/BoynerLibrary.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  MyLibrary
+//
+//  Created by Tugay on 19.12.2024.
+//
+
+import SwiftUI
+
+public struct BoynerLibrary: View {
+    public init() { }
+    public var body: some View {
+        Text("Boyner library")
+            .foregroundStyle(Color.red)
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    BoynerLibrary()
+}
