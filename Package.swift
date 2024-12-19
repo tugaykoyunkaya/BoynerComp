a5bcc1f0641c3a57190b52f8b0603208786d3020
Tugay <s.tugay.koyunkaya@gmail.com>
2024-12-19 12:04:44 +0300

    udpate

--- Package.swift
+++ Package.swift
@@ -11,12 +11,18 @@ let package = Package(
         .library(
             name: "BoynerComp",
             targets: ["BoynerComp"]),
+        .library(
+            name: "HorizonComp",
+            targets: ["HorizonComp"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BoynerComp"),
-
+            name: "BoynerComp",
+            path: "Sources/MyLibrary"),
+        .target(
+            name: "HorizonComp",
+            path: "HorizonComp"),
     ]
 )
